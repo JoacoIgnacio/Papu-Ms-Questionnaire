a1b5657b71f74b3359d8823e8c21b682540a5129
Joaquín Ignacio <joaquin.carvajal@alumnos.ucn.cl>
2024-09-09 18:04:52 -0300

    Arreglos

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,7 +4,7 @@ import { QuestionnaireModule } from './questionnaire/questionnaire.module';
 
 @Module({
   imports: [
-    MongooseModule.forRoot('mongodb://localhost:27017/mongodb'),  // URL de conexión a MongoDB
+    MongooseModule.forRoot('mongodb://localhost:27017/ms-questionnaire-mongodb'),  // URL de conexión a MongoDB
     QuestionnaireModule,  // Importa el módulo de cuestionarios
   ],
 })
