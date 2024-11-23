243d84e418987013e63880e146efbf983e9e7300
Joaquín Ignacio <joaquin.carvajal@alumnos.ucn.cl>
2024-11-23 02:19:53 -0300

    Implementación de la geolocalizacion
    Se cambiaron los schemas de questionnarieAnswer, se modifico createAnswerDto, se modifico los schemas de answer

--- src/answer/answer.service.ts
+++ src/answer/answer.service.ts
@@ -18,46 +18,53 @@ export class AnswerService {
     
   ) {}
 
-  async create(createAnswerDto: any): Promise<Answer> {
-    const { questionnaireId, questionId,userId, response, observations } = createAnswerDto;
+  async create(createAnswerDto: any): Promise<Answer | QuestionnaireAnswer> {
+    const { questionnaireId, userId, responses, location, images } = createAnswerDto;
 
-    // Verificar si la pregunta existe, si no, crearla
+    // Crear una nueva respuesta para el cuestionario
+    const createdAnswers = [];
 
-    let question = await this.questionModel.findById(questionId).exec();
+    // Crear las respuestas
+    for (let response of responses) {
+      const question = await this.questionModel.findById(response.questionId).exec();
 
-    if (!question) {
-      question = new this.questionModel({ _id: questionId, text: 'Nueva Pregunta', type: 'Tipo de pregunta' });
-      await question.save();
+      if (!question) {
+        throw new NotFoundException('Pregunta no encontrada');
+      }
+
+      const createdAnswer = new this.answerModel({
+        questionnaireAnswerId: null,
+        questionId: question._id,
+        userId: new Types.ObjectId(userId),
+        response: response.response,
+        observations: response.observations || null,
+        images, // Enviar las imágenes asociadas
+      });
+
+      await createdAnswer.save();
+      createdAnswers.push(createdAnswer);
     }
 
-    // Crear la respuesta
-    const createdAnswer = new this.answerModel({
-      questionnaireAnswerId: null, // Inicialmente null, se actualizará después
-      questionId: question._id,
-      userId: new Types.ObjectId(createAnswerDto.userId),
-      response: response,
-      observations,
-    });
-
-    // Relacionar la respuesta con el cuestionario
+    // Relacionar las respuestas con el cuestionario
     const now = new Date();
-    now.setSeconds(0, 0); // Eliminar segundos y milisegundos
-
-    now.setHours(now.getHours() - 3); // Restar 5 horas a la hora actual
+    now.setSeconds(0, 0);
 
     const questionnaireAnswer = new this.questionnaireAnswerModel({
       questionnaireId: new Types.ObjectId(questionnaireId),
-      answerId: createdAnswer._id,
+      answerId: createdAnswers.map(answer => answer._id),
       date: now.toISOString(),
+      location, // Guardar la ubicación aquí
     });
 
     await questionnaireAnswer.save();
 
-    // Asignar el questionnaireAnswerId al createdAnswer
-    createdAnswer.questionnaireAnswerId = questionnaireAnswer._id;
-    await createdAnswer.save();
+    // Asignar el questionnaireAnswerId a las respuestas creadas
+    for (let answer of createdAnswers) {
+      answer.questionnaireAnswerId = questionnaireAnswer._id;
+      await answer.save();
+    }
 
-    return createdAnswer;
+    return questionnaireAnswer;
   }
 
   // Obtener historial de cuestionarios completados por un usuario
